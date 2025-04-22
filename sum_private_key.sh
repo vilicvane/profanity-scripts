9e49fa9551ed9f1cc10f4d9b93ed001e90667d80
vilicvane <vilicvane@live.com>
2025-04-22 20:55:45 +0800

    File name

--- sum_private_key.sh
+++ sum_private_key.sh
@@ -1,9 +1,9 @@
-key_a=$(cat wallet_a.key | tr '[:lower:]' '[:upper:]')
-key_b=$(cat wallet_b.key | tr '[:lower:]' '[:upper:]')
+key_a=$(cat wallet.key_a | tr '[:lower:]' '[:upper:]')
+key_b=$(cat wallet.key_b | tr '[:lower:]' '[:upper:]')
 
 sum_expression="($key_a + $key_b) % FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F"
 
-key=$(echo "obase=16;ibase=16;$sum_expression" | bc)
+key=$(echo "obase=16;ibase=16;$sum_expression" | bc | tr '[:upper:]' '[:lower:]')
 
 key_padded=$(printf "%64s" $key | tr ' ' '0')
 
